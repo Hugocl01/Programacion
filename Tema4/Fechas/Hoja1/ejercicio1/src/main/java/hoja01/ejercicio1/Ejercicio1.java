981c8208bf267da9f26f0fc60be359985b2a0d34
hclIES <hugocayon@gmail.com>
2022-12-11 16:03:38 +0100

    Tema4

--- Tema4/Fechas/Hoja1/ejercicio1/src/main/java/hoja01/ejercicio1/Ejercicio1.java
+++ Tema4/Fechas/Hoja1/ejercicio1/src/main/java/hoja01/ejercicio1/Ejercicio1.java
@@ -12,6 +12,17 @@ package hoja01.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
+        //Creamos un array de la clase Letra
+        Letra arrayLetras[] = new Letra [3];
+        //Creamos un objeto en cada posicion
+        arrayLetras[0] = new Letra();
+        arrayLetras[1] = new Letra();
+        arrayLetras[2] = new Letra();
         
+        for (int i = 0; i < arrayLetras.length; i++) {
+            if(arrayLetras[i].vencida()) {
+                arrayLetras[i].mostrar();
+            }
+        }
     }
 }
