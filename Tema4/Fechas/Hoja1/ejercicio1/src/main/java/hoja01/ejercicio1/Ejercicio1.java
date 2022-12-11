b06168d5da825ace468b1570d1a9e90e1bb60db3
hclIES <hugocayon@gmail.com>
2022-12-11 16:13:39 +0100

    Tema4

--- Tema4/Fechas/Hoja1/ejercicio1/src/main/java/hoja01/ejercicio1/Ejercicio1.java
+++ Tema4/Fechas/Hoja1/ejercicio1/src/main/java/hoja01/ejercicio1/Ejercicio1.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package hoja01.ejercicio1;
 
 /**
@@ -13,15 +12,21 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
         //Creamos un array de la clase Letra
-        Letra arrayLetras[] = new Letra [3];
+        Letra arrayLetras[] = new Letra[3];
         //Creamos un objeto en cada posicion
         arrayLetras[0] = new Letra();
         arrayLetras[1] = new Letra();
         arrayLetras[2] = new Letra();
-        
+
         for (int i = 0; i < arrayLetras.length; i++) {
-            if(arrayLetras[i].vencida()) {
-                arrayLetras[i].mostrar();
+            if (arrayLetras[i].vencida()) {  //En caso de que la letra este vencida
+                System.out.println(arrayLetras[i].mostrar());   //Saca los datos de la letra
+                if () {
+                    arrayLetras[i].demora(15); //Demora 15 dias si la letra vencio en enero
+                }
+            } else {    //En caso de que la letra no este vencida
+                System.out.println(arrayLetras[i].getTitular());    //Saca el titular 
+                System.out.println(arrayLetras[i].diasFaltan());    //Saca el nº de dias que faltan
             }
         }
     }
---
+++ Tema4/Fechas/Hoja1/ejercicio2/src/main/java/hoja01/ejercicio2/Macota.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hoja01.ejercicio2;
+
+/**
+ *
+ * @author hugoc
+ */
+public class Macota {
+    
+}
